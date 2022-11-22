99d97aa4b3467ea4851a3ac7f9b48a6bf381660c
Brian Berns <brianberns@outlook.com>
2022-11-22 15:32:31 -0500

    Work on AST.

--- ZnutarCompiler/Syntax.fs
+++ ZnutarCompiler/Syntax.fs
@@ -6,22 +6,48 @@ type Literal =
     | IntLiteral of int
     | BoolLiteral of bool
 
-type Binop =
+type BinaryOperator =
     | Add | Sub | Mul | Eql
 
 type Variable = string
 
 type Expression =
     | VariableExpr of Variable
-    | ApplicationExpr of Expression * Expression
-    | LambdaExpr of Identifier * Expression
-    | LetExpr of Identifier * Expression * Expression
+    | ApplicationExpr of Application
+    | LambdaExpr of LambdaAbstraction
+    | LetExpr of LetBinding
     | LiteralExpr of Literal
-    | IfExpr of Expression * Expression * Expression
+    | IfExpr of If
     | FixExpr of Expression
-    | BinaryOperationExpr of Binop * Expression * Expression
+    | BinaryOperationExpr of BinaryOperator * Expression * Expression
+
+and Application =
+    {
+        Function : Expression
+        Argument : Expression
+    }
+
+and LambdaAbstraction =
+    {
+        Identifier : Identifier
+        Body : Expression
+    }
+
+and LetBinding =
+    {
+        Identifier : Identifier
+        Argument : Expression
+        Body : Expression
+    }
+
+and If =
+    {
+        Condition : Expression
+        TrueBranch : Expression
+        FalseBranch : Expression
+    }
 
 // let add x y = x + y; => let add = \x -> \y -> x + y;
-type Function = Identifier * Expression
+type Declaration = Identifier * Expression
 
-type Program = Program of List<Function> * Expression
+type Program = Program of List<Declaration> * Expression
