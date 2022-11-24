bd63f9de7485563d3c64dd644b4f2278099cbdfc
Brian Berns <brianberns@gmail.com>
2022-11-24 02:28:58 -0500

    Began work on interpreter.

---
+++ ZnutarCompiler/Eval.fs
@@ -0,0 +1,51 @@
+﻿namespace Znutar
+
+type Value =
+    | IntValue of int
+    | BoolValue of bool
+    | ClosureValue of Closure
+    
+and Closure =
+    {
+        Lambda : LambdaAbstraction
+        Environment : TermEnvironment
+    }
+
+and TermEnvironment = Map<Identifier, Value>
+
+module Eval =
+
+    let private evalBinaryOperation op xValue yValue =
+        match op, xValue, yValue with
+            | Plus, IntValue x, IntValue y -> Ok (IntValue (x + y))
+            | Minus, IntValue x, IntValue y -> Ok (IntValue (x - y))
+            | Times, IntValue x, IntValue y -> Ok (IntValue (x * y))
+            | Equals, IntValue x, IntValue y -> Ok (BoolValue (x = y))
+            | _ -> Error $"Invalid operation: {xValue} {op} {yValue}"
+
+    let rec evalExpr env expr =
+        result {
+            match expr with
+                | LiteralExpr (IntLiteral n) ->
+                    return IntValue n
+                | LiteralExpr (BoolLiteral b) ->
+                    return BoolValue b
+                | VariableExpr var ->
+                    match Map.tryFind var env with
+                        | Some value ->
+                            return value
+                        | None ->
+                            return! Error $"No such variable: {var}"
+                | BinaryOperationExpr bop ->
+                    let! leftVal = evalExpr env bop.Left
+                    let! rightVal = evalExpr env bop.Right
+                    return! evalBinaryOperation
+                        bop.Operator
+                        leftVal
+                        rightVal
+                | LambdaExpr lam ->
+                    return ClosureValue {
+                        Lambda = lam
+                        Environment = env
+                    }
+        }
---
+++ ZnutarCompiler/Result.fs
@@ -0,0 +1,15 @@
+﻿namespace Znutar
+
+open System
+
+type ResultBuilder() =
+    member _.Return(x) = Ok x
+    member _.ReturnFrom(res : Result<_, _>) = res
+    member _.Bind(res, f) = Result.bind f res
+    member _.Zero() = Ok ()
+
+[<AutoOpen>]
+module ResultBuilder =
+
+    /// Monadic result builder.
+    let result = ResultBuilder()
