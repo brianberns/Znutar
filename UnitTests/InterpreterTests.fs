c671288e0d343a7f466097880d9f6d6789974a05
Brian Berns <brianberns@gmail.com>
2022-11-30 23:20:46 -0500

    Modularized.

--- UnitTests/InterpreterTests.fs
+++ UnitTests/InterpreterTests.fs
@@ -2,20 +2,6 @@ namespace Znutar
 
 open Microsoft.VisualStudio.TestTools.UnitTesting
 
-type Assert private () =
-
-    // Improves error message for F# types (e.g. discriminated unions).
-    static member AreEqual<'t when 't : equality>(expected : 't, actual : 't) =
-        if actual <> expected then
-            sprintf "\nExpected: %A.\nActual:   %A" expected actual
-                |> Assert.Fail
-
-    // Improves error message for F# types (e.g. discriminated unions).
-    static member AreEqual<'t when 't : equality>(expected : 't, actual : 't, msg) =
-        if actual <> expected then
-            sprintf "%s\nExpected: %A.\nActual:   %A" msg expected actual
-                |> Assert.Fail
-
 [<TestClass>]
 type InterpreterTests() =
 
