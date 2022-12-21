13b6d84efc3a6f763c61e78ca857364964692cde
Brian Berns <brianberns@gmail.com>
2022-12-21 10:08:43 -0500

    Modularized.

--- ZnutarCompiler/Parser/Expression.fs
+++ ZnutarCompiler/Parser/Expression.fs
@@ -10,98 +10,104 @@ module Expression =
     let private parseExpression, private parseExpressionRef =
         createParserForwardedToRef ()
 
-    let private parseLambdaAbstraction =   // to-do: fold multiple arguments
-        parse {
-            do! skipString "fun" >>. spaces
-            let! ident = Identifier.parse
-            do! spaces >>. skipString "->" >>. spaces
-            let! body = parseExpression
-            return {
-                LambdaAbstraction.Identifier = ident
-                Body = body
+    module private SimpleExpr =
+
+        let private parseLambdaAbstraction =   // to-do: fold multiple arguments
+            parse {
+                do! skipString "fun" >>. spaces
+                let! ident = Identifier.parse
+                do! spaces >>. skipString "->" >>. spaces
+                let! body = parseExpression
+                return {
+                    LambdaAbstraction.Identifier = ident
+                    Body = body
+                }
             }
-        }
 
-    /// From: let f arg0 arg1 = binding in body
-    /// To:   let f = fun arg0 -> fun arg1 -> binding in body
-    let private parseLetBinding =
-        parse {
+        /// From: let f arg0 arg1 = binding in body
+        /// To:   let f = fun arg0 -> fun arg1 -> binding in body
+        let private parseLetBinding =
+            parse {
 
-            do! skipString "let" >>. spaces
-            let! recurisive =
-                opt (skipString "rec" .>> spaces)
-                    |>> Option.isSome
-            let! ident = Identifier.parse .>> spaces
-            let! argIdents = many (Identifier.parse .>> spaces)
-            do! skipChar '=' >>. spaces
-            let! binding = parseExpression
-            do! spaces >>. skipString "in" >>. spaces
-            let! body = parseExpression
+                do! skipString "let" >>. spaces
+                let! recursive =
+                    opt (skipString "rec" .>> spaces)
+                        |>> Option.isSome
+                let! ident = Identifier.parse .>> spaces
+                let! argIdents = many (Identifier.parse .>> spaces)
+                do! skipChar '=' >>. spaces
+                let! binding = parseExpression
+                do! spaces >>. skipString "in" >>. spaces
+                let! body = parseExpression
 
-            let binding' =
-                (argIdents, binding)
-                    ||> Seq.foldBack (fun argIdent acc ->
-                        LambdaExpr {
-                            Identifier = argIdent
-                            Body = acc
-                        })
-            return {
-                Recursive = recurisive
-                Identifier = ident
-                Argument = binding'
-                Body = body
+                let binding' =
+                    (argIdents, binding)
+                        ||> Seq.foldBack (fun argIdent acc ->
+                            LambdaExpr {
+                                Identifier = argIdent
+                                Body = acc
+                            })
+                return {
+                    Recursive = recursive
+                    Identifier = ident
+                    Argument = binding'
+                    Body = body
+                }
             }
-        }
 
-    let private parseLiteral =
-        choice [
-            pint32 |>> IntLiteral
-            skipString "true" >>% BoolLiteral true
-            skipString "false" >>% BoolLiteral false
-        ]
+        let private parseLiteral =
+            choice [
+                pint32 |>> IntLiteral
+                skipString "true" >>% BoolLiteral true
+                skipString "false" >>% BoolLiteral false
+            ]
 
-    let private parseIf =
-        parse {
-            do! skipString "if" >>. spaces
-            let! cond = parseExpression
-            do! spaces >>. skipString "then" >>. spaces
-            let! trueBranch = parseExpression
-            do! spaces >>. skipString "else" >>. spaces
-            let! falseBranch = parseExpression
-            return {
-                Condition = cond
-                TrueBranch = trueBranch
-                FalseBranch = falseBranch
+        let private parseIf =
+            parse {
+                do! skipString "if" >>. spaces
+                let! cond = parseExpression
+                do! spaces >>. skipString "then" >>. spaces
+                let! trueBranch = parseExpression
+                do! spaces >>. skipString "else" >>. spaces
+                let! falseBranch = parseExpression
+                return {
+                    Condition = cond
+                    TrueBranch = trueBranch
+                    FalseBranch = falseBranch
+                }
             }
-        }
 
-    let private parseAnnotation =
-        parse {
-            let! expr = parseExpression
-            do! spaces >>. skipChar ':' >>. spaces
-            let! typ = Type.parse
-            return {
-                Expression = expr
-                Type = typ
+        let private parseAnnotation =
+            parse {
+                let! expr = parseExpression
+                do! spaces >>. skipChar ':' >>. spaces
+                let! typ = Type.parse
+                return {
+                    Expression = expr
+                    Type = typ
+                }
             }
-        }
-            |> parseParens
-            |> attempt
+                |> parseParens
+                |> attempt
 
-    let private parseParenExpression =
-        parseParens parseExpression
+        let private parseParenExpression =
+            parseParens parseExpression
 
-    let private parseSimpleExpr : Parser<_, _> =
-        choice [
-            Identifier.parse |>> VariableExpr
-            parseLambdaAbstraction |>> LambdaExpr
-            parseLetBinding |>> LetExpr
-            parseLiteral |>> LiteralExpr
-            parseIf |>> IfExpr
-            parseAnnotation |>> AnnotationExpr
-            parseParenExpression
-        ]
+        /// Parses a simple (non-left-recursive) expression.
+        let parse : Parser<_, _> =
+            choice [
+                Identifier.parse |>> VariableExpr
+                parseLambdaAbstraction |>> LambdaExpr
+                parseLetBinding |>> LetExpr
+                parseLiteral |>> LiteralExpr
+                parseIf |>> IfExpr
+                parseAnnotation |>> AnnotationExpr
+                parseParenExpression
+            ]
 
+    /// Parses one or more simple expressions, folding them
+    /// together as function applications.
+    /// E.g. "a b c" -> "((a b) c)".
     let private parseSimpleExprs =
 
         let gather = function
@@ -115,7 +121,7 @@ module Expression =
                                 Argument = arg
                             })
 
-        many1 (parseSimpleExpr .>> spaces)
+        many1 (SimpleExpr.parse .>> spaces)
             |>> gather
 
     let private parseExprImpl =
