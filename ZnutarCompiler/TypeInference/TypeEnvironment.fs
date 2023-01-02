9de99984bf4c387daa12d4ab873a53059a5ff6ac
Brian Berns <brianberns@gmail.com>
2023-01-02 17:37:56 -0500

    Modularized.

---
+++ ZnutarCompiler/TypeInference/TypeEnvironment.fs
@@ -0,0 +1,26 @@
+﻿namespace Znutar.TypeInference
+
+open Znutar
+
+type private TypeEnvironment = Map<Identifier, Scheme>
+
+module private TypeEnvironment =
+
+    let empty : TypeEnvironment = Map.empty
+
+    let add ident scheme (env : TypeEnvironment) : TypeEnvironment =
+        env |> Map.add ident scheme
+
+    let tryFind ident (env : TypeEnvironment) =
+        match Map.tryFind ident env with
+            | Some scheme ->
+                Ok scheme
+            | None ->
+                Error (UnboundIdentifier ident)
+
+    /// Free type variables in the given environment.
+    let freeTypeVariables (env : TypeEnvironment) =
+        Seq.collect
+            Scheme.freeTypeVariables
+            (Map.values env)
+            |> set
