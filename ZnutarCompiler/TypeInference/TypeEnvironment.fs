1ad3e076cc13406ca4072fcd5a756e4622629f94
Brian Berns <brianberns@gmail.com>
2023-01-24 23:59:58 -0500

    Commented.

--- ZnutarCompiler/TypeInference/TypeEnvironment.fs
+++ ZnutarCompiler/TypeInference/TypeEnvironment.fs
@@ -2,15 +2,21 @@
 
 open Znutar
 
+/// Tracks schemes by name.
+/// E.g. "const" is mapped to <'a, 'b>('a -> 'b -> 'a).
 type private TypeEnvironment = Map<Identifier, Scheme>
 
 module private TypeEnvironment =
 
+    /// Empty type environment.
     let empty : TypeEnvironment = Map.empty
 
+    /// Adds the given scheme with the given identifier to
+    /// the given environment.
     let add ident scheme (env : TypeEnvironment) : TypeEnvironment =
         env |> Map.add ident scheme
 
+    /// Tries to find a scheme by name in the given environment.
     let tryFind ident (env : TypeEnvironment) =
         match Map.tryFind ident env with
             | Some scheme ->
